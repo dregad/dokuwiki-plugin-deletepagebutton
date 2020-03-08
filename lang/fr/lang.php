7814c669f62a542864f51a87c3c9b1d153b807cc
Damien Regad <dregad@mantisbt.org>
2020-03-08 22:43:07 +0100

    French translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Delete Page Button plugin - French language file
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Damien Regad
+ */
+
+$lang['menu_item']  = 'Supprimer cette page';
+$lang['deleted_ok'] = 'La page a été supprimée';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = 'Voulez-vous vraiment supprimer cette page ?';
+$lang['js']['btn_ok']     = 'OK';
+$lang['js']['btn_cancel'] = 'Annuler';
