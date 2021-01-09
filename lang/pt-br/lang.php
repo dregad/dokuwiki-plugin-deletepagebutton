4d0003a34f9453b4bfd1126ece862e673e8e48b1
Eduardo Mozart de Oliveira <2974895+coldscientist@users.noreply.github.com>
2021-01-08 21:44:47 -0300

    Portuguese-Brazil translation (#3)

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Delete Page Button plugin - Portuguese-Brazil language file
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Eduardo Mozart de Oliveira
+ */
+
+$lang['menu_item']  = 'Excluir página';
+$lang['deleted_ok'] = 'Página excluída com sucesso';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = 'Tem certeza que deseja excluir esta página ?';
+$lang['js']['btn_ok']     = 'OK';
+$lang['js']['btn_cancel'] = 'Cancelar';
