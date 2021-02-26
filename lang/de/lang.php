94a50311fa38b837983c0a154921a2f8b678838e
Frieder Schrempf <dev@fris.de>
2021-02-26 15:51:42 +0100

    Add German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Delete Page Button plugin - German language file
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Frieder Schrempf
+ */
+
+$lang['menu_item']  = 'Seite löschen';
+$lang['deleted_ok'] = 'Seite erfolgreich gelöscht';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = 'Soll die Seite wirklich gelöscht werden?';
+$lang['js']['btn_ok']     = 'OK';
+$lang['js']['btn_cancel'] = 'Abbrechen';
