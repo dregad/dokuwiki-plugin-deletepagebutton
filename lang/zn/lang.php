6046ba772dee4fcd93bdf6511e005f29002e2c7f
nukewarrior <journeyang@gmail.com>
2022-06-05 01:47:20 +0800

    Add Chinese localization

---
+++ lang/zn/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Delete Page Button plugin - Chinese language file
+ */
+
+$lang['menu_item']  = '删除页面';
+$lang['deleted_ok'] = '页面删除成功';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = '确认删除此页面?';
+$lang['js']['btn_ok']     = '确认';
+$lang['js']['btn_cancel'] = '取消';
