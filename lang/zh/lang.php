490076befb7fda3ae7ae2155b498463fed168beb
nukewarrior <journeyang@gmail.com>
2022-06-08 14:47:11 +0800

    Add Chinese localization (#12)

---
+++ lang/zh/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Delete Page Button plugin - Chinese language file
+ * 
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  yang
+ */
+
+$lang['menu_item']  = '删除页面';
+$lang['deleted_ok'] = '页面删除成功';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = '确认删除此页面?';
+$lang['js']['btn_ok']     = '确认';
+$lang['js']['btn_cancel'] = '取消';
