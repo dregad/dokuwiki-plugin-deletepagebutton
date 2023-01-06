6c045a123260a92ca46f7e434c7e9dcb4143dae1
xboxones1 <91512529+xboxones1@users.noreply.github.com>
2023-01-06 17:46:00 +0900

    Add Russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Delete Page Button plugin - Russian language file
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Damien Regad
+ */
+
+$lang['menu_item']  = 'Удалить страницу';
+$lang['deleted_ok'] = 'Страница удалена';
+
+$lang['js']['title']      = $lang['menu_item'];
+$lang['js']['confirm']    = 'Вы уверены, что хотите удалить страницу?';
+$lang['js']['btn_ok']     = 'OK';
+$lang['js']['btn_cancel'] = 'Отмена';
